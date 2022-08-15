4105d95dc8d320f22d1ddfc7afb56c3353b6d32f
Tung Nguyen <developer.tungnguyen@gmail.com>
2022-08-15 09:23:30 +0700

    integrate with ather-id

---
+++ src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import "../styles/globals.css";
+import "@sipher.dev/ather-id/public/assets/styles/globals.css";
+
+import type { AppProps } from "next/app";
+import { AtherIdProvider, AtherIdEnviromment } from "@sipher.dev/ather-id";
+import { Web3WalletProvider } from "@sipher.dev/web3";
+import { AuthProvider } from "../providers/auth";
+import { Web3AuthProvider } from "../services/web3auth";
+
+const atherIdOptions = {
+  environment: AtherIdEnviromment.Dev,
+  oauth: {
+    redirectSignIn: "http://localhost:3000/onboarding",
+    redirectSignOut: "http://localhost:3000/onboarding",
+  },
+};
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Web3WalletProvider>
+      <AtherIdProvider {...atherIdOptions}>
+        <AuthProvider>
+          <Web3AuthProvider chain="mainnet" web3AuthNetwork="testnet">
+            <Component {...pageProps} />
+          </Web3AuthProvider>
+        </AuthProvider>
+      </AtherIdProvider>
+    </Web3WalletProvider>
+  );
+}
+
+export default MyApp;
---
+++ src/providers/auth.tsx
@@ -0,0 +1,75 @@
+import { createContext, FC, useContext, useEffect } from "react";
+import { useRouter } from "next/router";
+import { AtherIdWidgetProvider, useAtherIdContext } from "@sipher.dev/ather-id";
+import { useToast } from "@chakra-ui/react";
+
+const useAuthValue = () => {
+  const atherId = useAtherIdContext();
+  const router = useRouter();
+
+  const { authState } = atherId;
+
+  useEffect(() => {
+    if (authState === "initializing" || !router.isReady) return;
+
+    const isLoginRoute = router.pathname === "/onboarding";
+    if (authState === "signIn" && !isLoginRoute) {
+      const next = encodeURIComponent(router.route);
+      router.push(`${"/onboarding"}?next=${next}`);
+    }
+    if (authState === "signedIn") {
+      if (isLoginRoute) {
+        let next = decodeURIComponent((router.query["next"] as string) || "/");
+        const [pathname, search] = next.split("?");
+        router.push({ pathname, search });
+      }
+    }
+  }, [router.isReady, router.pathname, authState]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      (window as any).LOG_LEVEL = "DEBUG";
+    }
+  }, []);
+
+  return {
+    ...atherId,
+    initializing: authState === "initializing",
+    authenticated: authState === "signedIn",
+  };
+};
+
+export type AuthContext = ReturnType<typeof useAuthValue>;
+
+const context = createContext<AuthContext | null>(null);
+
+export const AuthProvider: FC = ({ children }) => {
+  const value = useAuthValue();
+  const toast = useToast();
+  const handleError = (err: any) => {
+    toast({
+      id: err?.code,
+      status: "error",
+      title: err?.code,
+      description: err?.message,
+    });
+  };
+
+  return (
+    <context.Provider value={value}>
+      <AtherIdWidgetProvider onError={handleError}>
+        {children}
+      </AtherIdWidgetProvider>
+    </context.Provider>
+  );
+};
+
+export const useAuthContext = () => {
+  const value = useContext(context);
+
+  if (!value) {
+    throw new Error("useAuthContect must be used inside AuthProvider");
+  }
+
+  return value;
+};
